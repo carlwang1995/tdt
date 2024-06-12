68db3fdb15a20571299cd5cefd4de28853978dd0
Wei-Yao Wang <z5n9800z@gmail.com>
2024-06-12 13:21:55 +0800

    Fix 'prevent repeat loading' feture, and some CSS code. 12/06/24

--- static/js/app.js
+++ static/js/app.js
@@ -4,6 +4,7 @@ let btn = document.querySelector("#search_btn");
 let left_btn = document.querySelector("#left");
 let right_btn = document.querySelector("#right");
 let mrt_section = document.querySelector("#mrts");
+let bottom = document.querySelector("#bottom");
 let nextPage = null;
 let target = "/api/attractions?";
 
@@ -30,8 +31,8 @@ const intersectionObserver = new IntersectionObserver((entries) => {
   }
   const load_nextPage = async () => {
     if (nextPage != null) {
+      bottom.style.display = "none";
       let resopnse = await fetch(`${target}page=${nextPage}`);
-      console.log(`${target}page=${nextPage}`);
       let result = await resopnse.json();
       let data = result["data"];
       nextPage = result["nextPage"];
@@ -44,9 +45,15 @@ const intersectionObserver = new IntersectionObserver((entries) => {
       }
     }
   };
-  load_nextPage();
+  const show_bottom = async () => {
+    await load_nextPage();
+    if (nextPage != null) {
+      bottom.style.display = "block";
+    }
+  };
+  show_bottom();
 });
-intersectionObserver.observe(document.querySelector("#bottom"));
+intersectionObserver.observe(bottom);
 
 // 景點搜尋功能
 btn.addEventListener("click", async () => {
@@ -70,7 +77,7 @@ btn.addEventListener("click", async () => {
 });
 
 // mrt
-let get_mrts = async () => {
+const get_mrts = async () => {
   let resopnse = await fetch("/api/mrts");
   let result = await resopnse.json();
   let data = result["data"];
@@ -80,7 +87,7 @@ let get_mrts = async () => {
   }
 };
 
-let select_mrt_element = async () => {
+const select_mrt_element = async () => {
   await get_mrts();
   let mrt_element = document.querySelectorAll(".mrt");
   mrt_element.forEach((mrt) => {
